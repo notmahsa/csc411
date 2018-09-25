41624ca612d82835d72c36288a15b8e1024b1164
Mahsa Naserifar <mahsa@uber.com>
2018-09-25 17:37:38 -0400

    **ADDED** prediction

--- a1/hw1_code.py
+++ a1/hw1_code.py
@@ -15,15 +15,27 @@ np.random.shuffle(data)
 
 N = len(data)
 
-count_vectorizer = text.CountVectorizer(stop_words='english')
+count_vectorizer = text.CountVectorizer()
 
-train_d = count_vectorizer.fit([i[0] for i in data[:int(N*0.7)]])
-vdxn_d = count_vectorizer.fit([i[0] for i in data[int(N*0.7):int(N*0.85)]])
-test_d = count_vectorizer.fit([i[0] for i in data[int(N*0.85):]])
-train_t = text.CountVectorizer([i[1] for i in data[:int(N*0.7)]])
-vdxn_t = text.CountVectorizer([i[1] for i in data[int(N*0.7):int(N*0.85)]])
-test_t = text.CountVectorizer([i[1] for i in data[int(N*0.85):]])
+train_d = [i[0] for i in data[:int(N*0.7)]]
+vdxn_d = [i[0] for i in data[int(N*0.7):int(N*0.85)]]
+test_d = [i[0] for i in data[int(N*0.85):]]
+
+train_counts = count_vectorizer.fit_transform(train_d)
+vdxn_counts = count_vectorizer.transform(vdxn_d)
+test_counts = count_vectorizer.transform(test_d)
+
+train_t = [i[1] for i in data[:int(N*0.7)]]
+vdxn_t = [i[1] for i in data[int(N*0.7):int(N*0.85)]]
+test_t = [i[1] for i in data[int(N*0.85):]]
 
 tfidf_vectorizer = TfidfVectorizer(stop_words='english')
 
 clf = dtc(max_depth=3)
+clf.fit(X=train_counts, y=train_t)
+clf.score(X=vdxn_counts, y=vdxn_t)
+
+
+clf = dtc(max_depth=1)
+clf.fit(X=train_counts, y=train_t)
+clf.score(X=vdxn_counts, y=vdxn_t)
