410c5e5f5d3ba5bf9d12443ad0b394e96982dba0
Mahsa Naserifar <mahsa@uber.com>
2018-09-26 09:42:34 -0400

    **ADDED** part c, determinism, criteria and depths, visualization

--- a1/hw1_code.py
+++ a1/hw1_code.py
@@ -3,6 +3,7 @@ import scipy as sc
 import sklearn
 from sklearn.feature_extraction import text
 from sklearn.tree import DecisionTreeClassifier as dtc
+from sklearn.tree import export_graphviz as eg
 
 with open('clean_real.txt') as f:
     real = f.read().splitlines()
@@ -29,13 +30,20 @@ train_t = [i[1] for i in data[:int(N*0.7)]]
 vdxn_t = [i[1] for i in data[int(N*0.7):int(N*0.85)]]
 test_t = [i[1] for i in data[int(N*0.85):]]
 
-tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+max_depths = [1, 10, 100, 1000, 10000]
+criteria = ["gini", "entropy"]
 
-clf = dtc(max_depth=3)
-clf.fit(X=train_counts, y=train_t)
-clf.score(X=vdxn_counts, y=vdxn_t)
+accuracy = []
+trees = []
+for d in max_depths:
+    for c in criteria:
+        clf = dtc(criterion=c, max_depth=d, random_state=0)
+        oppress_output = clf.fit(X=train_counts, y=train_t)
+        # To validate accuracy calculated: accuracy.append(clf.score(X=vdxn_counts, y=vdxn_t))
+        pred = clf.predict(X=vdxn_counts)
+        accuracy.append(1 - np.mean(pred!=vdxn_t))
+        trees.append(clf)
 
-
-clf = dtc(max_depth=1)
-clf.fit(X=train_counts, y=train_t)
-clf.score(X=vdxn_counts, y=vdxn_t)
+M = np.argmax(accuracy)
+eg(trees[M], out_file='tree.dot', max_depth=2)
+print M
