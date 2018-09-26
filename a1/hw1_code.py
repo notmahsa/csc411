19f049ee36b70f35fe90f715789948540e422108
Mahsa Naserifar <mahsa@uber.com>
2018-09-26 10:55:17 -0400

    **MODULARIZED** script and added feature and class names

--- a1/hw1_code.py
+++ a1/hw1_code.py
@@ -5,45 +5,64 @@ from sklearn.feature_extraction import text
 from sklearn.tree import DecisionTreeClassifier as dtc
 from sklearn.tree import export_graphviz as eg
 
-with open('clean_real.txt') as f:
-    real = f.read().splitlines()
 
-with open('clean_fake.txt') as f:
-    fake = f.read().splitlines()
+def load_data():
+    # load headlines
+    with open('clean_real.txt') as f:
+        real = f.read().splitlines()
+    with open('clean_fake.txt') as f:
+        fake = f.read().splitlines()
 
-data = np.array([[i, 1] for i in real] + [[i, 0] for i in fake[:]])
-np.random.shuffle(data)
+    # flag headlines with Real=1 and Fake=0 and shuffle them in one array
+    data = np.array([[i, 1] for i in real] + [[i, 0] for i in fake[:]])
+    np.random.shuffle(data)
+    N = len(data)
+    count_vectorizer = text.CountVectorizer()
 
-N = len(data)
+    # separate training, validation, and test data
+    train_d = [i[0] for i in data[:int(N*0.7)]]
+    vdxn_d = [i[0] for i in data[int(N*0.7):int(N*0.85)]]
+    test_d = [i[0] for i in data[int(N*0.85):]]
 
-count_vectorizer = text.CountVectorizer()
+    # vectorize data
+    train_counts = count_vectorizer.fit_transform(train_d)
+    vdxn_counts = count_vectorizer.transform(vdxn_d)
+    test_counts = count_vectorizer.transform(test_d)
 
-train_d = [i[0] for i in data[:int(N*0.7)]]
-vdxn_d = [i[0] for i in data[int(N*0.7):int(N*0.85)]]
-test_d = [i[0] for i in data[int(N*0.85):]]
+    # separate training, validation and test targets
+    train_t = [i[1] for i in data[:int(N*0.7)]]
+    vdxn_t = [i[1] for i in data[int(N*0.7):int(N*0.85)]]
+    test_t = [i[1] for i in data[int(N*0.85):]]
 
-train_counts = count_vectorizer.fit_transform(train_d)
-vdxn_counts = count_vectorizer.transform(vdxn_d)
-test_counts = count_vectorizer.transform(test_d)
+    return train_counts, vdxn_counts, test_counts, \
+           train_t, vdxn_t, test_t, count_vectorizer.get_feature_names()
 
-train_t = [i[1] for i in data[:int(N*0.7)]]
-vdxn_t = [i[1] for i in data[int(N*0.7):int(N*0.85)]]
-test_t = [i[1] for i in data[int(N*0.85):]]
 
-max_depths = [1, 10, 100, 1000, 10000]
-criteria = ["gini", "entropy"]
+def select_model(train_counts, vdxn_counts, test_counts,
+                 train_t, vdxn_t, test_t, features):
+    max_depths = [1, 10, 50, 100, 200]
+    criteria = ["gini", "entropy"]
+    accuracy = []
+    trees = []
 
-accuracy = []
-trees = []
-for d in max_depths:
-    for c in criteria:
-        clf = dtc(criterion=c, max_depth=d, random_state=0)
-        oppress_output = clf.fit(X=train_counts, y=train_t)
-        # To validate accuracy calculated: accuracy.append(clf.score(X=vdxn_counts, y=vdxn_t))
-        pred = clf.predict(X=vdxn_counts)
-        accuracy.append(1 - np.mean(pred!=vdxn_t))
-        trees.append(clf)
+    # iterate through max_depth values and critera to calculate accuracy
+    for d in max_depths:
+        for c in criteria:
+            clf = dtc(criterion=c, max_depth=d, random_state=0)
+            oppress_output = clf.fit(X=train_counts, y=train_t)
+            pred = clf.predict(X=vdxn_counts)
+            # calculate accuracy as "1 - error"
+            accuracy.append(1 - np.mean(pred!=vdxn_t))
+            print "Criterion:", c, "& Max Depth:", d, "| Accuracy:", 1 - np.mean(pred!=vdxn_t)
+            trees.append(clf)
 
-M = np.argmax(accuracy)
-eg(trees[M], out_file='tree.dot', max_depth=2)
-print M
+    # find tree with maximum accuracy
+    M = np.argmax(accuracy)
+    # visualize tree as a .dot file
+    eg(trees[M], out_file='tree.dot', max_depth=2,
+       feature_names=features, class_names=["Fake","Real"])
+
+
+if __name__ == "__main__":
+    tc, vc, sc, tt, vt, st, features = load_data()
+    select_model(tc, vc, sc, tt, vt, st, features)
